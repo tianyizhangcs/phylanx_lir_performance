a5d88ce85084fde89255f4dc2ea61584b06ff101
harter019@gmail.com <harter019@gmail.com>
2018-06-14 08:57:08 -0500

    adding results

---
+++ 04JUNE2018/LIR_PYTHON/RunLirPy1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "This script is used to run lir_csv_python example"
+
+
+iteration_array=(1 100 1000 5000 10000)
+row_stop_array=(7000 8000 9000 10000)
+thr=(1 2 4 6 8 10 12 14 16 18 20)
+
+#iteration_array=(1 1000)
+#row_stop_array=(100 1000)
+#thr=(1 2)
+for th in "${thr[@]}"
+    do
+    for it in "${iteration_array[@]}"
+        do
+        for rs in "${row_stop_array[@]}"
+            do		
+            echo "doing ${th}_${it}_${rs}_thitrs"	
+            export OMP_NUM_THREADS=${th}   
+            python3 lir_csv_python.py $it 0 $rs 0 $rs >> lirpy_${th}th_itrscs_${it}_${rs}
+            echo "done ${th}_${it}_${rs}"	
+	    done
+        done
+    done
+
+
+
